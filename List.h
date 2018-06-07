d6921753c18ff82d074d2d01357fecda4fcc3cb4
shay halimi <shayhalimi@gmail.com>
2018-06-07 23:49:57 +0300

    רשימה מלאה. תיקנתי בצומת דבר או שניים. עוד לא עשיתי בדיקות

--- List.h
+++ List.h
@@ -11,9 +11,9 @@ class Node{
     Node<T>* next;
     Node<T>* prev;
 public:
-    Node(const T& data) : data(data){}
+    Node(const T& data) : data(data), next(NULL), prev(NULL){}
     const T& getData(){ return data;}
-    void setData(const T& data){this->data = data};
+    void setData(const T& data){this->data = data;};
     Node<T>* getNext(){ return next;}
     void setNext(Node<T>* next){this->next = next;}
     Node<T>* getPrev(){ return prev;}
@@ -21,19 +21,53 @@ public:
 };
 
 //not complete
-template <typename T>
+template <typename T, typename CMP>
 class List{
     Node<T>* head;
     int size;
 public:
-    List() : head(new Node()), size(0){} ////????/////
+    List(const T& emptyValue) : head(new Node<T>(emptyValue)), size(0){}
     ~List(){
         destroyList(head);
     }
     void destroyList(Node<T>* node){
         if(node==NULL){
-
+            return;
         }
+        destroyList(node->getNext());
+        delete node;
+    }
+    void insert(const T& data){
+        Node<T>* node = new Node<T>(data);
+        node->setNext(head->getNext());
+        node->getPrev(head);
+        head->setNext(node);
+    }
+    template <typename CMP>
+    Node<T>* find(const T& data){
+        Node<T>* curr = head;
+        Node<T>* key = new Node<T>(data);
+        while (CMP(curr, key) == false){
+            curr = curr->getNext();
+            if (curr == NULL){
+                return NULL;
+            }
+        }
+        delete key;
+        return curr;
+    }
+    bool destroy(const T& data){
+        Node<T>* node = find(data);
+        if (node == NULL){
+            return false;
+        }
+        Node<T>* prev = node->getPrev();
+        node<T>* next = node->getNext();
+        prev->setNext(next);
+        if(next != NULL){
+            next->setPrev(prev);
+        }
+        return true;
     }
 };
 #endif //PROJECT_LIST_H
